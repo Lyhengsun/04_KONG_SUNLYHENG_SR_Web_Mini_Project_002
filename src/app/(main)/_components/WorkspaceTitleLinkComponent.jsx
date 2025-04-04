1b948d1dad8f107f97324fc91591743541d70f48
Lyhengsun <101932930+Lyhengsun@users.noreply.github.com>
2025-04-04 13:34:04 +0700

    added random color to workspace list

--- src/app/(main)/_components/WorkspaceTitleLinkComponent.jsx
+++ src/app/(main)/_components/WorkspaceTitleLinkComponent.jsx
@@ -3,13 +3,20 @@ import Link from "next/link";
 import React from "react";
 
 const WorkspaceTitleLinkComponent = ({ workspace }) => {
+  const colors = ["red", "blue", "green", "yellow", "orange", "purple"];
+
+  const color = colors[Math.floor(Math.random() * colors.length)];
+
   return (
     <Link
       href={`/workspace/${workspace.workspaceId}`}
       className="flex items-center justify-between w-full px-8 py-2 hover:bg-gray-200"
     >
       <div className="flex items-center gap-x-2">
-        <div className="w-2 aspect-square rounded-full bg-black mx-2"></div>
+        <div
+          className="w-2 aspect-square rounded-full mx-2"
+          style={{ backgroundColor: color }}
+        ></div>
         <p className="text-xl font-medium font-sf-pro-display">
           {workspace.workspaceName}
         </p>
